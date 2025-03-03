528fd81729a62ff327eefa1d57b8e77384058cf8
lainel john dela cruz <ljbdelacruz123@gmail.com>
2025-03-03 10:49:58 +0800

    workflow update fix

--- script/unittest.sh
+++ script/unittest.sh
@@ -3,5 +3,5 @@
 # assign python path
 PYTHON_PATH=$(which python3)
 
-# ran unit test service
-PYTHON_PATH -m unittest discover -s tests/service -p "test_*.py"
+# run unit test service
+$PYTHON_PATH -m unittest discover -s tests/service -p "test_*.py"
