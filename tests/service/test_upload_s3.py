dd8116e69b608121aad0c944ec6e97e579529a2e
lainel john dela cruz <ljbdelacruz123@gmail.com>
2025-03-03 11:14:55 +0800

    unit test update

--- tests/service/test_upload_s3.py
+++ tests/service/test_upload_s3.py
@@ -1,8 +1,10 @@
+import os
 import unittest
 from unittest.mock import patch, MagicMock
 from src.service.upload_s3 import upload_single_image
-import os
 
+
+AWS_BUCKET_NAME = os.getenv('AWS_BUCKET')
 class TestUploadS3(unittest.TestCase):
 
     @patch('src.service.upload_s3.s3_client.put_object')
@@ -17,9 +19,6 @@ class TestUploadS3(unittest.TestCase):
         # Mock file object
         file_obj = MagicMock()
         file_obj.filename = 'test_image.png'
-
-        # Set the environment variable for the S3 bucket name
-        os.environ['AWS_BUCKET_NAME'] = 'laineltestbucket'
 
         # Call the function
         url, file_key = upload_single_image(file_obj, object_name='test_image.png')
